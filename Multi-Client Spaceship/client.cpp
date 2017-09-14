3492fa94c12f7233becf0584b39788e1f32d2fef
Rapange <h_ero@hotmail.es>
2017-09-14 11:04:22 -0500

    Matriz

--- Multi-Client Spaceship/client.cpp
+++ Multi-Client Spaceship/client.cpp
@@ -16,13 +16,17 @@
 
   typedef vector<char> vch;
 
-  #define MAX_ACTION 7
-  #define MAX_SIZE 10
+  #define MAX_ACTION 8
+  #define MAX_HIT 4
+  #define MAX_SIZE 20
+#define MAX_CAPACITY 1025
   
   char gameArea[50][50];
   vector<vch> myMap;
+vector<vector<unsigned int> > game_matrix;
   char num_player;
   int xx, yy;
+bool dead = false;
 
   void initializeMap(vector<vch>& myMap){
   vch dummy;
@@ -35,6 +39,38 @@
   }
   }
 
+void updateMatrix(string my_matrix){
+  vector<unsigned int> holder;
+  string num;
+  bool anterior = false;
+  game_matrix.clear();
+  for(unsigned int i = 0; i < my_matrix.size(); i++){
+    if(my_matrix[i] == ','){
+      holder.push_back(stoi(num));
+      num = "";
+    }
+    else if(my_matrix[i] == ';'){
+      if(my_matrix[i-1] != ';') holder.push_back(stoi(num));
+      game_matrix.push_back(holder);
+      holder.clear();
+      num = "";
+    }
+    else{
+      num.push_back(my_matrix[i]);
+    }
+  }
+  game_matrix.pop_back();
+}
+
+void printGameMatrix(){
+  for(unsigned int i = 0; i < game_matrix.size(); i++){
+    for(unsigned int j = 0; j < game_matrix[i].size(); j++){
+      cout<<game_matrix[i][j]<<" ";
+    }
+    cout<<endl;
+  }
+}
+
 void drawMap(){
   for(int i = 0; i < myMap.size(); i++){
     for(int j = 0; j < myMap.size(); j++){
@@ -44,8 +80,34 @@ void drawMap(){
   }
 }
 
-void updateBullet(int x, int y, char dir){
+char getNumPlayer(char letterPlayer){
+  string abc = "-ABC";
+  char index;
+  for(unsigned int i = 0; i < abc.size(); i++){
+    if(abc[i] == letterPlayer){
+      index = char(i + '0');
+      return index;
+    }
+  }
+  return '-';
+}
+
+char hurtOrKilled(char letterPlayer){
+  char letterPlayerNum = getNumPlayer(letterPlayer);
+  unsigned int i = (letterPlayerNum - '0') - 1;
+  unsigned int sum = 0;
+  cout<<"i "<<i<<" filas: "<<game_matrix[0].size()<<endl;
+  for(unsigned int j = 0; j < game_matrix.size(); j++){
+    sum += game_matrix[j][i];
+  }
+  cout<<"suma: "<<sum<<endl;
+  if(sum < 3) return 'H';
+  return 'K';
+}
+
+void updateBullet(int x, int y, char dir, char myPlayer, int socketFD){
   bool start = true;
+  char protocol[MAX_HIT];
   while(x > 0 && x < (MAX_SIZE-1) && y > 0 && y < (MAX_SIZE-1)){
     if(myMap[x][y]=='.') myMap[x][y] = ' ';
 
@@ -73,6 +135,19 @@ void updateBullet(int x, int y, char dir){
       x--, y++;
     }
     start = false;
+    if(myMap[x][y] != ' ' && myMap[x][y] != '.'){
+      if(num_player == myPlayer){
+	protocol[0] = myPlayer;
+	protocol[1] = hurtOrKilled(myMap[x][y]);
+	protocol[2] = getNumPlayer(myMap[x][y]);
+	protocol[3] = '\0';
+	int n = write(socketFD,protocol,MAX_HIT);
+	printf("BALA: %s", protocol);
+      }
+      
+      myMap[x][y] = ' ';
+      break;
+    }
     myMap[x][y] = '.';
     drawMap();
     sleep(1);
@@ -120,32 +195,67 @@ void updateMap(vector<vch>& myMap,int x, int y, int player){
   }
 
   void readSD(int clientSD){
-    string x,y,player;
-    char protocol[MAX_ACTION];
+    string x,y,player, strProtocol;
+    char protocol[MAX_CAPACITY];
     int n;
     while(true){
-      n = read(clientSD,protocol,MAX_ACTION);
-      printf("Mi protocolo %s \n",protocol);
+      bzero(protocol,MAX_CAPACITY);
+      n = read(clientSD,protocol,2);
+      printf("Mi read protocolo %s \n",protocol);
 
+      strProtocol = protocol[0];
+      strProtocol += protocol[1];
+      cout<<"Leo inicial: "<<strProtocol<<endl;
       player = protocol[0];
-      if(protocol[1] == '0' && num_player == '-') num_player = protocol[0];
-      x = protocol[2];
-      x+= protocol[3];
-
-      y = protocol[4];
-      y+= protocol[5];
-
-      if(protocol[1] != '2'){
-	updateMap(myMap, stoi(x), stoi(y), stoi(player));
-	drawMap();
+      if(protocol[1] == '0' && num_player == '-') {
+	num_player = protocol[0];
+	cout<<"HOla"<<endl;
       }
-      else if(protocol[1] == '2'){
-	std::thread(updateBullet,stoi(x),stoi(y),protocol[6]).detach();
-     
+
+      if(protocol[1] == '0' || protocol[1] == '1' || protocol[1] == '2'){
+	cout<<"leo"<<endl;
+	bzero(protocol, MAX_CAPACITY);
+	n = read(clientSD, protocol, MAX_ACTION - 2);
+	strProtocol += protocol;
+	cout<<"Mi protocolo leido todo es: "<<strProtocol<<endl;
+	strProtocol.copy(protocol, MAX_ACTION);
+	x = protocol[2];
+	x+= protocol[3];
+
+	y = protocol[4];
+	y+= protocol[5];
+	if(protocol[1] == '0' || protocol[1] == '1'){
+	  updateMap(myMap, stoi(x), stoi(y), stoi(player));
+	  drawMap();
+	}
+	else if(protocol[1] == '2'){
+	  std::thread(updateBullet,stoi(x),stoi(y),protocol[6], protocol[0],clientSD).detach();
+	}
+      }
+      
+      else if(protocol[1] == 'H' || protocol[1] == 'K'){
+	bzero(protocol, MAX_CAPACITY);
+	n = read(clientSD, protocol, MAX_HIT - 2);
+	strProtocol += protocol;
+	if(strProtocol[1] == 'K'){
+	  if(strProtocol[2] == num_player) dead = true;
+	}
+	strProtocol.copy(protocol, MAX_HIT);
+	cout<<strProtocol<<endl;
+      }
+      else if(protocol[1] == 'X'){
+	bzero(protocol, MAX_CAPACITY);
+	n = read(clientSD, protocol, 3);
+	strProtocol = protocol;
+	n = read(clientSD, protocol, stoi(strProtocol));
+	strProtocol = protocol;
+	updateMatrix(strProtocol);
+	printGameMatrix();
+	cout<<"La matrix es: "<<strProtocol<<endl;
       }
       //drawMap();
 	
-      bzero(protocol,MAX_ACTION);
+      bzero(protocol,MAX_CAPACITY);
     }
     shutdown(clientSD, SHUT_RDWR); 
     close(clientSD);
@@ -164,38 +274,42 @@ void updateMap(vector<vch>& myMap,int x, int y, int player){
     protocol[3] = '0';
     protocol[4] = '0';
     protocol[5] = '0';
-    protocol[6] = -'0';
+    protocol[6] = '0';
+    protocol[7] = '\0';
 
     n = write(clientSD,protocol,MAX_ACTION);
+    //printf("Mi write protocolo es: %s", protocol);
+    cin.ignore();
     while(true){
+      //cin.ignore(100,'\n');
+      bzero(protocol,MAX_ACTION);
       cin.getline(buffer,2);
+      //cin>>n;
 
-      protocol[1] = '1';
-      if(buffer[0] == 'e') xx--, protocol[6] = '1';
-      else if(buffer[0] == 'x') xx++, protocol[6] = '2';
-      else if(buffer[0] == 's') yy--, protocol[6] = '3';
-      else if(buffer[0] == 'd') yy++, protocol[6] = '4';
-      else protocol[6] = buffer[0], protocol[1] = '2';
-      x = to_string(xx);
-      y = to_string(yy);
-      if(x.size() == 1) x = '0' + x;
-      if(y.size() == 1) y = '0' + y;
-      protocol[0] = num_player;
-      //protocol[1] = '1';
-      protocol[2] = x[0];
-      protocol[3] = x[1];
-      protocol[4] = y[0];
-      protocol[5] = y[1];
-      //system("cls");
-      //gotKey = getch();
-      //switch(gotKey){
-      // case 72: printf("72");
-      //}
-      //
-      //string temp="123456";
-      //strcpy(protocol,temp.c_str());
-      printf("Mi protocolo es: %s", protocol);
-      n = write(clientSD,protocol,MAX_ACTION);
+      if(!dead){
+	protocol[1] = '1';
+	if(buffer[0] == 'e') xx--, protocol[6] = '1';
+	else if(buffer[0] == 'x') xx++, protocol[6] = '2';
+	else if(buffer[0] == 's') yy--, protocol[6] = '3';
+	else if(buffer[0] == 'd') yy++, protocol[6] = '4';
+	else protocol[6] = buffer[0], protocol[1] = '2';
+	x = to_string(xx);
+	y = to_string(yy);
+	if(x.size() == 1) x = '0' + x;
+	if(y.size() == 1) y = '0' + y;
+	protocol[0] = num_player;
+	//protocol[1] = '1';
+	protocol[2] = x[0];
+	protocol[3] = x[1];
+	protocol[4] = y[0];
+	protocol[5] = y[1];
+	protocol[7] = '\0';
+	printf("Mi write protocolo es: %s", protocol);
+	n = write(clientSD,protocol,MAX_ACTION);
+      }
+      
+
+     
       //updateGameArea();
     }
     shutdown(clientSD, SHUT_RDWR);
@@ -221,7 +335,10 @@ void updateMap(vector<vch>& myMap,int x, int y, int player){
     int n, port;
     bool disco = false;
     
- 
+    cout<<"Puerto: ";
+    cin>>port;
+    cout<<"IP: ";
+    cin>>IP;
     if (-1 == SocketFD)
     {
       perror("cannot create socket");
@@ -231,8 +348,8 @@ void updateMap(vector<vch>& myMap,int x, int y, int player){
     memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
  
     stSockAddr.sin_family = AF_INET;
-    stSockAddr.sin_port = htons(41000);
-    Res = inet_pton(AF_INET, "127.0.0.1", &stSockAddr.sin_addr);
+    stSockAddr.sin_port = htons(port);
+    Res = inet_pton(AF_INET, "192.168.1.108", &stSockAddr.sin_addr);
  
     if (0 > Res)
     {
@@ -275,29 +392,9 @@ void updateMap(vector<vch>& myMap,int x, int y, int player){
     yy = stoi(y);
     buffer = action;
     cout<<buffer<<endl;
-    //while(true)
-    //{
+  
     std::thread(writeSD,SocketFD).detach();   
     std::thread(readSD,SocketFD).detach();
-    //cin>>message;
-    //cin.getline(buffer,6);
-    //n= write(SocketFD,message.data(),message.size());
-    //n = write(SocketFD,buffer,6);
-    /* perform read write operations ... */
-    //char newbuffer[6];
-    //n = read(SocketFD,buffer,6);
-    // if (n < 0) perror("ERROR reading from socket");
-    // printf("%s \n",buffer);
-    //}
-    /**for(int i = 0 ; i < 50 ; ++i){
-      for(int j = 0 ; j <50 ; ++j){
-        gameArea[i][j]='.';
-      }
-    }
-    gameArea[25][25]='A';
-    gameArea[25][26]='A';
-    gameArea[26][25]='A';
-    gameArea[26][26]='A';**/
     while(true){
       
     }
@@ -307,4 +404,4 @@ void updateMap(vector<vch>& myMap,int x, int y, int player){
     //close(SocketFD);
 
     return 0;gameArea[25][25]='A';
-  }
+}
--- Multi-Client Spaceship/server.cpp
+++ Multi-Client Spaceship/server.cpp
@@ -1,4 +1,4 @@
-  /* Server code in C */
+/* Server code in C */
  
   #include <sys/types.h>
   #include <sys/socket.h>
@@ -11,11 +11,15 @@
   #include <thread>
   #include <string.h>
   #include <unistd.h>
+#include <iostream>
  
   using namespace std;
 
-  #define MAX_ACTION 7
+  #define MAX_ACTION 8
+  #define MAX_HK 4
+  #define MAX_CAPACITY 1025
   vector<int> clients;
+vector<vector<unsigned int> > game_matrix;
   int nextNewPlayer = 1;
 
   string intToStr (int x)
@@ -25,26 +29,102 @@
       return str.str();
   }
 
+inline unsigned int getSize(){
+  unsigned int tam = game_matrix.size();
+  return 1 + tam + ((tam - 1) * tam) + (tam * tam) + 1;
+}
+
+string OKson(){
+  string matrix;
+  for(unsigned int i = 0; i < game_matrix.size(); i++){
+    for(unsigned int j = 0; j < game_matrix[i].size(); j++){
+      matrix += intToStr(game_matrix[i][j]) + ",";
+    }
+    matrix.pop_back();
+    matrix += ";";
+  }
+  matrix += ";";
+  return matrix;
+}
+
+void updateMatrix(char protocol[3]){
+  unsigned int i = protocol[0] - '0';
+  unsigned int j = protocol[2] - '0';
+  game_matrix[i][j]++;
+}
+
+void updateMatrixKilled(char endP){
+  unsigned int i = endP - '0' - 1;
+  game_matrix.erase(game_matrix.begin()+i);
+  for(unsigned int j = 0; j < game_matrix.size(); j++){
+    game_matrix[j].erase(game_matrix[j].begin()+i);
+  }
+
+  for(unsigned int i = 0; i < game_matrix.size(); i++){
+    for(unsigned int j = 0; j < game_matrix.size(); j++){
+      cout<<game_matrix[i][j]<<" ";
+    }
+    cout<<endl;
+  }
+}
+
+string buildMatrixProtocol(){
+  string strProtocol = intToStr(getSize());
+  while(strProtocol.size() < 3){
+      strProtocol = "0" + strProtocol;
+  }
+
+  strProtocol = "1X" + strProtocol + OKson();
+  return strProtocol;
+}
+
   void bot(int clientSD)
   {
-    char protocol[MAX_ACTION];
-    string intToString;
+    char protocol[MAX_CAPACITY];
+    //char matrix_protocol[MAX_CAPACITY];
+    string intToString, strProtocol;
     char sendProtocol[MAX_ACTION];
     int n;
+    bool nuevo_cliente = false;
 
     while(true){
-
-      n = read(clientSD,protocol,MAX_ACTION);
+      nuevo_cliente = false;
+      bzero(protocol,MAX_CAPACITY);
+      n = read(clientSD,protocol,2);
       if (n < 0) perror("ERROR reading from socket");
-      //printf("Here is the message: [%s]\n",buffer);
-      //buffer2 = buffer;
-      //buffer2 = "You Send: " + buffer2;
-      //strcpy(buffer,buffer2.c_str());
-      //holdProtocol = protocol;
-      //strcpy(sendProtocol,holdProtocol.c_str());    
+      strProtocol = protocol[0];
+      strProtocol += protocol[1];
+      cout<<"Leyendo dos: "<<strProtocol<<endl;
 
+      
+      if(protocol[1] == '0' || protocol[1] == '1' || protocol[1] == '2'){
+	bzero(protocol,MAX_CAPACITY);
+	n = read(clientSD, protocol, MAX_ACTION - 2);
+
+	if(n < 0) perror("ERROR reading from socket");
+      }
+      else if(protocol[1] == 'H' || protocol[1] == 'K'){
+	bzero(protocol,MAX_CAPACITY);
+	n = read(clientSD, protocol, MAX_HK - 2);
+        game_matrix[strProtocol[0] - '0' - 1][protocol[0] - '0' - 1]++;
+	if(strProtocol[1] == 'K'){
+	  updateMatrixKilled(protocol[0]);
+	}
+	if(n < 0) perror("ERROR reading from socket");
+      }
+      /*else if(protocol[1] == 'X'){
+	n = read(clientSD, protocl, MAX_ACTION);
+	if(n < 0) perror("ERROR reading from socket");
+      }*/
+
+      strProtocol += protocol;
+      //bzero(protocol,MAX_CAPACITY);
+      strProtocol.copy(protocol,strProtocol.size());
+
+      cout<<"Leyendo todo: "<<strProtocol<<endl;
       for (int i=0;i<clients.size();i++){
         if(protocol[0] == '0'){
+	  nuevo_cliente = true;
           printf("newUser");
           intToString = intToStr(nextNewPlayer);
           protocol[0] = intToString[0];
@@ -54,12 +134,46 @@
           protocol[4] = '0';
           protocol[5] = '0';
 	  protocol[6] = '0';
+	  protocol[7] = '\0';
           nextNewPlayer++;
+
+	  printf("Escribiendo: %s", protocol);
           n = write(clients[i],protocol,MAX_ACTION);
-	  
+	  strProtocol = buildMatrixProtocol();
+
+	  char matrix_protocol[strProtocol.size()];
+	  bzero(matrix_protocol,strProtocol.size());
+	  strProtocol.copy(matrix_protocol, strProtocol.size());
+
+	  cout<<"TE envio cliente: "<<strProtocol<<endl;  
+	  n = write(clients[i],matrix_protocol,strProtocol.size());
         }
         else{
-          n = write(clients[i],protocol,MAX_ACTION);
+	  if(protocol[1] == 'H' || protocol[1] == 'K'){
+	    n = write(clients[i],protocol,MAX_HK);
+
+	    strProtocol = buildMatrixProtocol();
+
+	    char matrix_protocol[strProtocol.size()];
+	    bzero(matrix_protocol,strProtocol.size());
+	    strProtocol.copy(matrix_protocol, strProtocol.size());
+
+	    
+	    n = write(clients[i],matrix_protocol,strProtocol.size());
+	  }
+	  
+	  else{
+	    n = write(clients[i],protocol,MAX_ACTION);
+	    if(nuevo_cliente){
+	      strProtocol = buildMatrixProtocol();
+
+	      char matrix_protocol[strProtocol.size()];
+	      bzero(matrix_protocol,strProtocol.size());
+	      strProtocol.copy(matrix_protocol, strProtocol.size());
+	      n = write(clients[i],matrix_protocol,strProtocol.size());
+	    }
+	  }
+          
           //bzero(buffer, sizeof(buffer));
           if (n < 0) perror("ERROR writing to socket");
         }
@@ -74,11 +188,15 @@
 
   int main()
   {
+    vector<unsigned int> holder;
     struct sockaddr_in stSockAddr;
     int SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
     int ConnectFD = 0;
     //char buffer[256];
-    //int n;
+    int port;
+
+    cout<<"Puerto: ";
+    cin>>port;
  
     if(-1 == SocketFD)
     {
@@ -89,7 +207,7 @@
     memset(&stSockAddr, 0, sizeof(struct sockaddr_in));
  
     stSockAddr.sin_family = AF_INET;
-    stSockAddr.sin_port = htons(41000);
+    stSockAddr.sin_port = htons(port);
     stSockAddr.sin_addr.s_addr = INADDR_ANY;
  
     if(-1 == bind(SocketFD,(const struct sockaddr *)&stSockAddr, sizeof(struct sockaddr_in)))
@@ -118,21 +236,18 @@
       }
     
     clients.push_back(ConnectFD);
+    holder.clear();
+    for(unsigned int i = 0; i < game_matrix.size(); i++){
+      holder.push_back(0);
+      game_matrix[i].push_back(0);
+    }
+    holder.push_back(0);
+    game_matrix.push_back(holder);
     std::thread(bot,ConnectFD).detach();
     
-     //n = read(ConnectFD,buffer,255);
-     //if (n < 0) perror("ERROR reading from socket");
-     //printf("Here is the message: [%s]\n",buffer);
-     //n = write(ConnectFD,"I got your message",18);
-     //if (n < 0) perror("ERROR writing to socket");
- 
-     /* perform read write operations ... */
- 
-      //shutdown(ConnectFD, SHUT_RDWR);
- 
-      //close(ConnectFD);
+     
     }
  
     close(SocketFD);
     return 0;
-  }
+}
